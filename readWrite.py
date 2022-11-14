49e4fc16548b7a48e4596ad477b7fac9055a3501
evankski <evankarwowski@gmail.com>
2022-11-13 17:42:38 -0800

    .

---
+++ readWrite.py
@@ -0,0 +1,9 @@
+
+# When using open use ''
+file = open('test.txt')
+
+# this will read all contents of the file
+print(file.read())
+
+# Always close the file at the end of the program
+file.close()
